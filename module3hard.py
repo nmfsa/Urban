8275acccebbb1ec25f788bbd4b980c19cbe73960
nmfsa <167591806+nmfsa@users.noreply.github.com>
2024-05-16 05:19:42 +0300

    Update module3hard.py

--- module3hard.py
+++ module3hard.py
@@ -1,29 +1,27 @@
 data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
-     (6, {'cube': 7, 'drum': 8}),
-     "Hello",
-     ((), [{(2, 'Urban', ('Urban2', 35))}])
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 
 def calculate_structure_sum(*args):
-    sum = 0
+    sum_ = 0
     for i in args:
         for j in i:
             if isinstance(j, dict):
                 for key, value in j.items():
-                    sum += len(key) + value
+                    sum_ += len(key) + value
             elif isinstance(j, int):
-                sum += j
+                sum_ += j
             elif isinstance(j, str):
-                sum += len(j)
+                sum_ += len(j)
             else:
-                 sum += calculate_structure_sum(j)
-    return sum
+                sum_ += calculate_structure_sum(j)
+    return sum_
 
 
 result = calculate_structure_sum(data_structure)
 print(result)
-
-
