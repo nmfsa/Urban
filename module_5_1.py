ac6ea6553eecc2869d683842d6816c5549f200be
nmfsa <167591806+nmfsa@users.noreply.github.com>
2024-05-27 06:02:01 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,16 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(1, new_floor+1):
+                print(i)
+
+
+dom = House('ЖК Видный', 5)
+dom.go_to(5)
+dom.go_to(10)
