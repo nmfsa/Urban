6dc55a502e6837ce9ee53d23db8c31f6cc37dc82
nmfsa <167591806+nmfsa@users.noreply.github.com>
2024-06-19 18:01:43 +0300

    Update Multiple_inheritance.py

--- Multiple_inheritance.py
+++ Multiple_inheritance.py
@@ -1,14 +1,13 @@
 class Vehicle:
 
-    def __init__(self, price, vehicle_type = "none"):
+    def __init__(self, price, vehicle_type="none"):
         self.vehicle_type = vehicle_type
         super().__init__(price)
 
 
-
 class Car:
 
-    def __init__(self,  price=1000000):
+    def __init__(self, price=1000000):
         self.price = price
 
     def horse_powers(self, power):
@@ -21,10 +20,7 @@ class Nissan(Vehicle, Car):
         super().__init__(price, vehicle_type)
 
 
-
 avto1 = Nissan(5000, 'Nissan')
 print(avto1.price)
 print(avto1.vehicle_type)
 print(avto1.horse_powers(123))
-
-
