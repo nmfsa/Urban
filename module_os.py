87ecaf882f347c1ac02967e562c89f1d4f31901c
nmfsa <167591806+nmfsa@users.noreply.github.com>
2024-07-18 07:39:06 +0300

    Add files via upload

---
+++ module_os.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+for root, dirs, files in os.walk("."):
+    for file in files:
+        filepath = os.path.join(os.getcwd(), file)
+        filetime = os.path.getmtime(file)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(file)
+        parent_dir = os.path.dirname(filepath)
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
+            f'Родительская директория: {parent_dir}')
